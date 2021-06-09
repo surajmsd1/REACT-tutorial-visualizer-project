7c3fa1a8d5f34d12ef8a898c973b908630368717
suraj kopparam <surajmsd1@gmail.com>
2021-06-08 23:47:35 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react';
+import TodoList from './TodoList'
 function App() {
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          yoo Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TodoList todos = {todos} />
+      <input type="text"/>
+      <button> Add Todo</button>
+      <button>Clear Completed</button>
+      <div> 0 left to do</div>
+    </>
+
+  )
 }
 
 export default App;
